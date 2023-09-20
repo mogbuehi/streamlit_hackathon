e805c956911665e5dd48799d2129fbc3872c3d83
mogbuehi <38775809+mogbuehi@users.noreply.github.com>
2023-09-19 21:00:36 -0700

    Update app.py

--- app.py
+++ app.py
@@ -188,7 +188,7 @@ def split_audio_wave(audio_path, clip_duration_sec=30, overlap_duration_sec=3, o
 
 # Takes WAV audio clips and transcribes them and saves them to JSON
 def transcribe(audio_path):
-    clip_path_list = split_audio(audio_path)
+    clip_path_list = split_audio_wave(audio_path)
     file_name = os.path.basename(audio_path)[:-4]
     transcribed_clips = []
     # Ensure the transcript file is empty to start with
